ce4b347453a0a1833f273409b10e9987f97ed2ac
Ihor Bahniuk <hattorikanto@gmail.com>
2024-06-01 03:31:26 +0300

    deleted gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://hattorikanto.github.io/todo-app/",
-  build: {
-    outDir: "dist",
-  },
-});
+})
